0807d6350cee91747f7f62f2b22d7901934f54ee
Arthur Polon <arthurpolon@hotmail.com>
2021-06-05 14:28:43 -0400

    Added auth functions.

--- contexts/AuthContext.js
+++ contexts/AuthContext.js
@@ -1,21 +1,58 @@
-import React, {createContext, useContext} from 'react';
-import { auth } from '../lib/firebase'
-import { useAuthState } from "react-firebase-hooks/auth"
+import React, { createContext, useContext, useState } from 'react';
+import { auth } from '../lib/firebase';
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
-function AuthContextProvider ({children}) {
-  const [ user, loading, error ] = useAuthState(auth)
+function AuthContextProvider({ children }) {
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [currentUser, setCurrentUser] = useState(null);
+  auth.onAuthStateChanged((user) => {
+    if (user) {
+      setCurrentUser(user);
+      setError(null);
+    }
+  });
+
+  const createUser = (email, password) => {
+    auth.createUserWithEmailAndPassword(email, password);
+  };
+
+  const signInUser = (email, password) => {
+    auth.signInWithEmailAndPassword(email, password).catch((err) => {
+      setError(err.message);
+      setTimeout(() => {
+        setError(null);
+      }, 3000);
+    });
+  };
+
+  const signOutUser = () => {
+    auth
+      .signOut()
+      .then(console.log('user logged out'))
+      .then(setCurrentUser(null));
+  };
 
   return (
-    <AuthContext.Provider value={{user, loading, error}}>
+    <AuthContext.Provider
+      value={{
+        currentUser,
+        loading,
+        error,
+        createUser,
+        signInUser,
+        signOutUser,
+        setError,
+      }}
+    >
       {children}
     </AuthContext.Provider>
-  )
+  );
 }
 
 export const useAuth = () => {
-  return useContext(AuthContext)
-}
+  return useContext(AuthContext);
+};
 
 export default AuthContextProvider;
