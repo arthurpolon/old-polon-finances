6aa6b3da1a7aeaf1534584a3332951ee7e9bc33c
Arthur Polon <arthurpolon@hotmail.com>
2021-06-17 15:10:21 -0400

    page formatted with prettier

--- pages/index.js
+++ pages/index.js
@@ -13,30 +13,31 @@ import {
   SimpleGrid,
   VStack,
 } from '@chakra-ui/react';
-import { FiLayout, FiDollarSign, FiLock } from "react-icons/fi";
-import NextLink from 'next/link'
-import Navbar from '../components/Navbar'
-import Footer from '../components/Footer'
+import { FiLayout, FiDollarSign, FiLock } from 'react-icons/fi';
+import NextLink from 'next/link';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
 
 export default function Home() {
-  const { colorMode } = useColorMode()
+  const { colorMode } = useColorMode();
   return (
     <>
       <Navbar />
-      
+
       {/* Hero */}
-      <Container maxW={'7xl'} minH="calc(85vh - 107px)">
+      <Container maxW={'7xl'} minH='calc(85vh - 107px)'>
         <Stack
           align={'center'}
           spacing={{ base: 8, md: 10 }}
           pt={{ base: 10, md: 28 }}
           direction={{ base: 'column', md: 'row' }}
-          >
+        >
           <Stack flex={1} spacing={{ base: 5, md: 10 }}>
             <Heading
               lineHeight={1.1}
               fontWeight={600}
-              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}>
+              fontSize={{ base: '3xl', sm: '4xl', lg: '6xl' }}
+            >
               <Text
                 as={'span'}
                 position={'relative'}
@@ -49,7 +50,8 @@ export default function Home() {
                   left: 0,
                   bg: 'green.400',
                   zIndex: -1,
-                }}>
+                }}
+              >
                 Your money
               </Text>
               <br />
@@ -59,13 +61,14 @@ export default function Home() {
             </Heading>
             <Text color={'gray.500'}>
               Polon-Finance$ is an app to handle all your incomes and expenses.
-              That way, you can see, save and plan your financial life in a simple way.
-              Click below and start planning your future!
+              That way, you can see, save and plan your financial life in a
+              simple way. Click below and start planning your future!
             </Text>
             <Stack
               spacing={{ base: 4, sm: 6 }}
-              direction={{ base: 'column', sm: 'row' }}>
-              <NextLink href="/app">
+              direction={{ base: 'column', sm: 'row' }}
+            >
+              <NextLink href='/app'>
                 <a>
                   <Button
                     rounded={'lg'}
@@ -73,7 +76,7 @@ export default function Home() {
                     fontWeight={'normal'}
                     px={6}
                     colorScheme={'green'}
-                    >
+                  >
                     Get started
                   </Button>
                 </a>
@@ -85,7 +88,8 @@ export default function Home() {
             justify={'center'}
             align={'center'}
             position={'relative'}
-            w={'full'}>
+            w={'full'}
+          >
             <Blob
               w={'150%'}
               h={'150%'}
@@ -101,7 +105,8 @@ export default function Home() {
               rounded={'2xl'}
               boxShadow={'2xl'}
               width={'full'}
-              overflow={'hidden'}>
+              overflow={'hidden'}
+            >
               <Image
                 alt={'Hero Image'}
                 fit={'cover'}
@@ -118,8 +123,8 @@ export default function Home() {
       </Container>
 
       {/* Bottom Cards */}
-      <Box maxW="70%" mx="auto" p={4}>
-        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{base: 10, md: 20}}>
+      <Box maxW='70%' mx='auto' p={4}>
+        <SimpleGrid columns={{ base: 1, md: 3 }} spacing={{ base: 10, md: 20 }}>
           <Feature
             icon={<Icon as={FiLayout} w={10} h={10} />}
             title={'Easy Interface'}
@@ -146,22 +151,23 @@ export default function Home() {
 
       <Footer />
     </>
-  )
+  );
 }
 
 export const Blob = (props) => {
   return (
     <Icon
       width={'100%'}
-      viewBox="0 0 578 440"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}>
+      viewBox='0 0 578 440'
+      fill='none'
+      xmlns='http://www.w3.org/2000/svg'
+      {...props}
+    >
       <path
-        fillRule="evenodd"
-        clipRule="evenodd"
-        d="M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z"
-        fill="currentColor"
+        fillRule='evenodd'
+        clipRule='evenodd'
+        d='M239.184 439.443c-55.13-5.419-110.241-21.365-151.074-58.767C42.307 338.722-7.478 282.729.938 221.217c8.433-61.644 78.896-91.048 126.871-130.712 34.337-28.388 70.198-51.348 112.004-66.78C282.34 8.024 325.382-3.369 370.518.904c54.019 5.115 112.774 10.886 150.881 49.482 39.916 40.427 49.421 100.753 53.385 157.402 4.13 59.015 11.255 128.44-30.444 170.44-41.383 41.683-111.6 19.106-169.213 30.663-46.68 9.364-88.56 35.21-135.943 30.551z'
+        fill='currentColor'
       />
     </Icon>
   );
@@ -169,7 +175,7 @@ export const Blob = (props) => {
 
 const Feature = ({ title, text, icon }) => {
   return (
-    <VStack align="center">
+    <VStack align='center'>
       <Flex
         w={16}
         h={16}
@@ -177,11 +183,14 @@ const Feature = ({ title, text, icon }) => {
         justify={'center'}
         rounded={'full'}
         bg={useColorModeValue('gray.200', 'gray.700')}
-        mb={1}>
+        mb={1}
+      >
         {icon}
       </Flex>
       <Text fontWeight={600}>{title}</Text>
-      <Text align="center" color={'gray.600'}>{text}</Text>
+      <Text align='center' color={'gray.600'}>
+        {text}
+      </Text>
     </VStack>
   );
 };
