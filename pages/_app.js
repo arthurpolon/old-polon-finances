cc527e2eb010976e2f190b982121822e0d8f49a7
Arthur Polon <arthurpolon@hotmail.com>
2021-06-22 19:31:57 -0400

    Added FuegoProvider to app

--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,30 @@
-import { ChakraProvider } from "@chakra-ui/react"
-import Head from 'next/head'
-import AppContextProvider from "../contexts/AppContext"
+import { ChakraProvider } from '@chakra-ui/react';
+import { FuegoProvider } from '@nandorojo/swr-firestore';
+import { Fuego } from '../lib/fuego-swr';
+import { firebaseConfig } from '../lib/firebase';
+import Head from 'next/head';
+import AppContextProvider from '../contexts/AppContext';
+import '@fontsource/poppins/200.css';
+import '@fontsource/poppins/400.css';
+import '@fontsource/poppins/600.css';
+import theme from '../styles/theme';
 
-import "@fontsource/poppins/200.css"
-import "@fontsource/poppins/400.css"
-import "@fontsource/poppins/600.css"
-import theme from '../styles/theme'
+const fuego = new Fuego(firebaseConfig);
 
 function MyApp({ Component, pageProps }) {
   return (
     <ChakraProvider theme={theme}>
-      <AppContextProvider>
-        <Head>
-          <title>Polon-Finance$</title>
-          <meta name="description" content="Homepage" />
-          <link rel="icon" href="/favicon.svg"/>
-        </Head>
-        <Component {...pageProps} />
-      </AppContextProvider>
+      <FuegoProvider fuego={fuego}>
+        <AppContextProvider>
+          <Head>
+            <title>Polon-Finance$</title>
+            <meta name='description' content='Homepage' />
+            <link rel='icon' href='/favicon.svg' />
+          </Head>
+          <Component {...pageProps} />
+        </AppContextProvider>
+      </FuegoProvider>
     </ChakraProvider>
-  )
+  );
 }
-export default MyApp
+export default MyApp;
